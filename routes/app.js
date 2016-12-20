f61b1299ea44ea211438806fb2447445a93a5f23
Yvan Pearson <ypearson@nklabs.com>
2016-12-20 17:06:40 -0500

    Starting section 4 of course.

--- routes/app.js
+++ routes/app.js
@@ -1,8 +1,31 @@
+
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user')
 
 router.get('/', function (req, res, next) {
-    res.render('index');
+    User.findOne({}, function(err, doc) {
+    	if(err) {
+    		return res.send('Error');
+    	}
+    	res.render('node', { email: doc.email });
+
+    })
+});
+
+router.post('/', function(req, res, next) {
+
+	var email = req.body.email;
+	var user = new User({
+		firstName:'Max',
+		lastName: 'Pearson',
+		password: 'password',
+		email: email
+	});
+
+	user.save();
+	res.redirect('/');
+
 });
 
 module.exports = router;
