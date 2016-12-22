627ee49b9e064c5ea99d88aa332b5e6ca91cd882
Yvan Pearson <ypearson@nklabs.com>
2016-12-22 16:17:11 -0500

    doing angular section.

--- routes/app.js
+++ routes/app.js
@@ -1,30 +1,10 @@
 
 var express = require('express');
 var router = express.Router();
-var User = require('../models/user')
 
 router.get('/', function (req, res, next) {
-    User.findOne({}, function(err, doc) {
-    	if(err) {
-    		return res.send('Error');
-    	}
-    	res.render('node', { email: doc.email });
 
-    })
-});
-
-router.post('/', function(req, res, next) {
-
-	var email = req.body.email;
-	var user = new User({
-		firstName:'Max',
-		lastName: 'Pearson',
-		password: 'password',
-		email: email
-	});
-
-	user.save();
-	res.redirect('/');
+    res.render('index');
 
 });
 
